ac8da2589d26b5f227e4c7d7ecac4d9eafdb90cc
Andrey <135460452+Ancf01@users.noreply.github.com>
2024-11-18 19:47:21 -0300

    Mais javaScript

--- Projeto JS e-commerce/app.js
+++ Projeto JS e-commerce/app.js
@@ -1,7 +1,26 @@
-function fechar(){
-    document.getElementById("side-bar").style.display = "none";
-}
-function abrir(){
+var sideBar ="none";
 
-    document.getElementById("side-bar").style.display = "block";
+function abrir(){
+    
+    if(sideBar=== "block"){
+        sideBar = "none"
+    } else if (sideBar === "none"){
+        sideBar = "block"
+    }
+    document.getElementById("side-bar").style.display = sideBar;
 }
+
+
+window.onscroll = function(){navRolagem()};
+
+            function navRolagem(){
+                
+                if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+                    document.getElementById("cabeca").style.top = "0px";
+                    
+                    document.getElementById("cabeca").style.background = "rgba(180, 180, 180, 0.2)";
+                }else{
+                document.getElementById("cabeca").style.top="-5";
+                document.getElementById("cabeca").style.background ="white";
+                }
+        }
