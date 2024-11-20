dbc064dbc1ccad16800535875d1830b528322df1
Andrey <135460452+Ancf01@users.noreply.github.com>
2024-11-20 23:45:02 +0000

    Estudo dia 20

--- Projeto JS e-commerce/app.js
+++ Projeto JS e-commerce/app.js
@@ -19,8 +19,46 @@ window.onscroll = function(){navRolagem()};
                     document.getElementById("cabeca").style.top = "0px";
                     
                     document.getElementById("cabeca").style.background = "rgba(180, 180, 180, 0.2)";
+
+                    document.getElementById("bar-L").style.top = "0px";
+                    
+                    document.getElementById("bar-L").style.background = "rgba(180, 180, 180, 0.2)";
                 }else{
                 document.getElementById("cabeca").style.top="-5";
                 document.getElementById("cabeca").style.background ="white";
+
+                document.getElementById("bar-L").style.top="-5";
+                document.getElementById("bar-L").style.background ="white";
                 }
-        }
+            }
+            
+            slideIndex = 1;
+
+            mostrarSlides(slideIndex);
+            
+            function correrSlide(n){
+                mostrarSlides(slideIndex = n);
+            }
+            
+                function mostrarSlides(n){
+                    document.getElementsByClassName("carrosel");
+            
+                    document.getElementsByClassName("dot");
+            
+                    for (i = 0; i < slides.length; i++)
+                        {slides[i].style.display="none";}
+                                        
+            
+                    for (i = 0; i < carrousel.length; i++){carrousel[i].className = carrousel[i].className.replace( "active", "");
+            
+                    }
+            
+                    slides[n-1].style.display="block";
+                    carrousel[n-1].className += "active";
+            
+                }
+
+
+
+          
+        
